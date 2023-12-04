e43d42f85f41b8f3d2bf407cb17ba89feb2cc704
DominykasKupsys <dominykas.kupsys@stud.ktmc.lt>
2023-12-04 16:24:28 +0200

    NodePrekes uzduotis su ejs

---
+++ PrekesNodeSuEjs/app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const app = express();
+let ejs = require("ejs");
+const port = 8080;
+var { prekes } = require("./prekes");
+
+
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  ejs.renderFile(
+    "templates/prekes.ejs",
+    {
+      title: "Prekių sąrašas",
+      content: "Prekių sąrašas",
+      prekes: prekes
+    },
+    function (err, str) {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Server error");
+      } else {
+        res.send(str);
+      }
+    }
+  );
+});
+
+app.get("/prekes/:pavadinimas", (req, res) => {
+  let pavadinimas = req.params["pavadinimas"];
+  if (prekes[pavadinimas]) {
+    ejs.renderFile(
+      "templates/preke.ejs",
+      {
+        title: "Prekės sąrašas",
+        content: "Prekės sąrašas",
+        prekes: prekes,
+        pavadinimas: pavadinimas
+      },
+      function (err, str) {
+        if (err) {
+          console.log(err);
+          res.status(500).send("Server error");
+        } else {
+          res.send(str);
+        }
+      }
+    );
+  } else {
+    res.status(404).send("Prekės nerado");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Programa veikia per port ${port}`);
+  console.log(`http://localhost:${port}`);
+});
+
---
+++ PrekesNodeSuEjs/prekes.js
@@ -0,0 +1,65 @@
+exports.prekes = {
+  kompiuteris: {
+    pavadinimas: "Nešiojamas kompiuteris HP EliteBook 745 G6",
+    kaina: 261.59,
+    aprasymas: "Geros kokybės kompiuteris",
+    paveiksliukas:
+      "https://lt2.pigugroup.eu/colours/671/054/98/67105498/hp-elitebook-745-g6-amd-ryzen-3-c39bc_reference.jpg",
+  },
+  ausines: {
+    pavadinimas: "Belaidės ausinės JBL Tune 510BT",
+    kaina: 29.99,
+    aprasymas: `Viena iš išskirtinių „JBL“ ausinių savybių - belaidis ryšys. Naudodami „Bluetooth“ technologiją, 
+    šias ausines galite lengvai susieti su savo išmaniuoju telefonu,
+    planšetiniu kompiuteriu ar bet kuriuo kitu „Bluetooth“ palaikančiu įrenginiu.`,
+    paveiksliukas:
+      "https://lt2.pigugroup.eu/colours/409/497/78/40949778/belaides-ausines-jbl-tune-510-bt-t510btbl-internetu_reference.jpg",
+  },
+  klavetura: {
+    pavadinimas:
+      "Laidinė klaviatūra, Membraninė klaviatūra, Žaidimų klaviatūra Razer Ornata V3 X RGB",
+    kaina: 42.49,
+    aprasymas: "rgb klavetūra, skirta žaidimams",
+    paveiksliukas:
+      "https://lt2.pigugroup.eu/colours/582/863/03/58286303/razer-ornata-v3-x-us-wired-keyboard-43771_reference.jpg",
+  },
+  kede: {
+    pavadinimas: "Fury Avenger L",
+    kaina: 94.21,
+    aprasymas: "Avenger L visų pirma skirtas komfortui ir patogumui",
+    paveiksliukas:
+      "https://ksd-images.lt/display/aikido/store/cf0ea838f233086774a99d3864723a8f.jpg?h=742&w=816",
+  },
+  stalas: {
+    pavadinimas:
+      "Elektrinis reguliuojamo aukščio stalas Desktronic su USB A ir C jungtimis",
+    kaina: 419.0,
+    aprasymas: `„Desktronic“ meistriškai kūrėme savo stalą su aukštos kokybės elektriniu varikliu, atitinkančiu TÜV Rheinland reikalavimus,
+    kad reguliavimas būtų sklandus ir patikimas. `,
+    paveiksliukas:
+      "https://lt2.pigugroup.eu/colours/582/502/99/58250299/elektrinis-reguliuojamo-aukscio-stalas-desktronic-su-usb-4761f_reference.jpg",
+  },
+  pelyte: {
+    pavadinimas: "SteelSeries Rival 3 wireless ergonomic gaming mouse",
+    kaina: 44.0,
+    aprasymas: "belaidė žaidimų pelytė, su ilgai veikiančia baterija",
+    paveiksliukas:
+      "https://gameroom.lt/30188/steelseries-rival-3-wireless-ergonomic-gaming.jpg",
+  },
+  juosta: {
+    pavadinimas: "Smart LED juosta Gosund SL2",
+    kaina: 12.75,
+    aprasymas: `LED juostos - tai šiuolaikiškas požiūris į apšvietimą,
+    kuris leidžia stilingai, kokybiškai ir suvartojant mažiau elektros energijos apšviesti norimas erdves`,
+    paveiksliukas:
+      "https://lt2.pigugroup.eu/colours/484/686/62/48468662/smart-led-juosta-gosund-sl2-5m-07d20_reference.jpg",
+  },
+  gfuel: {
+    pavadinimas: "G FUEL",
+    kaina: 35.99,
+    aprasymas: `Dieviškasis eleksyras, tinkantis tik EPIC žaidėjams! Skonio pumpurus maloninantis bruknių (Lingonberry) skonio gūsis,
+    skandinaviškos kilmės! Įkvėptas tik pačio Felikso, PewDiePie ! Laikas tikram skoniui!`,
+    paveiksliukas:
+      "https://www.energyfuel.lt/cdn/shop/products/pewdiepiegfuel_700x.jpg?v=1686646053",
+  },
+};
